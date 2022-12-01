4310ecc60d9aa0f317b8f7f245bc02d5a4b03317
tsangsiu <tsangsiu@gmail.com>
2022-12-01 06:21:17 +0000

    Done Day 1

--- 2022/Day01/day01.rb
+++ 2022/Day01/day01.rb
@@ -1,1 +1,14 @@
+# input
+
 input = File.read("input.txt")
+input = input.split("\n\n").map { |input| input.split("\n") }
+calories = input.map { |calories| calories.map(&:to_i) }
+
+# part 1
+
+calories = calories.map { |calories| calories.sum }
+p calories.max
+
+# part 2
+
+p calories.max(3).sum
