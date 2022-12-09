e61bc6136ad149fb2bd8b628165a0edd6c79e211
tsangsiu <tsangsiu@gmail.com>
2022-12-09 20:17:29 +0800

    Done Day 9

---
+++ 2022/Day09/day09.rb
@@ -0,0 +1,217 @@
+# input
+
+motions = File.read("input.txt").split("\n").map { |pair| [pair.split.first, pair.split.last.to_i] }
+
+# part 1
+
+def horizontal?(head_pos, tail_pos)
+  head_pos[1] == tail_pos[1]
+end
+
+def vertical?(head_pos, tail_pos)
+  head_pos[0] == tail_pos[0]
+end
+
+def overlap?(head_pos, tail_pos)
+  head_pos == tail_pos
+end
+
+def together?(head_pos, tail_pos)
+  ((head_pos[0] - tail_pos[0]).abs == 1 && horizontal?(head_pos, tail_pos)) || # horizontal
+  ((head_pos[1] - tail_pos[1]).abs == 1 && vertical?(head_pos, tail_pos)) || # vertical
+  ((head_pos[0] - tail_pos[0]).abs == 1 && (head_pos[1] - tail_pos[1]).abs == 1) || # diagonal
+  overlap?(head_pos, tail_pos) # overlap
+end
+
+def tail_new_diag_pos(head_pos, tail_pos)
+  x_head = head_pos[0]; y_head = head_pos[1]
+  x_tail = tail_pos[0]; y_tail = tail_pos[1]
+  
+  if x_head > x_tail
+    return [tail_pos[0] + 1, tail_pos[1] + 1] if y_head > y_tail
+    return [tail_pos[0] + 1, tail_pos[1] - 1] if y_head < y_tail
+  elsif x_head < x_tail
+    return [tail_pos[0] - 1, tail_pos[1] + 1] if y_head > y_tail
+    return [tail_pos[0] - 1, tail_pos[1] - 1] if y_head < y_tail
+  end
+end
+
+START = [0, 0]
+
+# part 1
+
+head_visited = [START]; tail_visited = [START]
+
+motions.each do |motion|
+  direction = motion.first
+  steps = motion.last
+
+  if direction == 'R'
+    steps.times do
+      head_new_pos = [head_visited.last[0] + 1, head_visited.last[1]]
+      
+      if horizontal?(head_new_pos, tail_visited.last) && !together?(head_new_pos, tail_visited.last)
+        tail_new_pos = [tail_visited.last[0] + 1, tail_visited.last[1]]
+      elsif together?(head_new_pos, tail_visited.last)
+        tail_new_pos = tail_visited.last
+      else
+        tail_new_pos = tail_new_diag_pos(head_new_pos, tail_visited.last)
+      end
+
+      head_visited << head_new_pos
+      tail_visited << tail_new_pos         
+    end
+  elsif direction == 'L'
+    steps.times do
+      head_new_pos = [head_visited.last[0] - 1, head_visited.last[1]]
+      
+      if horizontal?(head_new_pos, tail_visited.last) && !together?(head_new_pos, tail_visited.last)
+        tail_new_pos = [tail_visited.last[0] - 1, tail_visited.last[1]]
+      elsif together?(head_new_pos, tail_visited.last)
+        tail_new_pos = tail_visited.last
+      else
+        tail_new_pos = tail_new_diag_pos(head_new_pos, tail_visited.last)
+      end
+      
+      head_visited << head_new_pos
+      tail_visited << tail_new_pos
+    end
+  elsif direction == 'U'
+    steps.times do
+      head_new_pos = [head_visited.last[0], head_visited.last[1] + 1]
+      
+      if vertical?(head_new_pos, tail_visited.last) && !together?(head_new_pos, tail_visited.last)
+        tail_new_pos = [tail_visited.last[0], tail_visited.last[1] + 1]
+      elsif together?(head_new_pos, tail_visited.last)
+        tail_new_pos = tail_visited.last
+      else
+        tail_new_pos = tail_new_diag_pos(head_new_pos, tail_visited.last)
+      end
+      
+      head_visited << head_new_pos
+      tail_visited << tail_new_pos
+    end
+  elsif direction == 'D'
+    steps.times do
+      head_new_pos = [head_visited.last[0], head_visited.last[1] - 1]
+      
+      if vertical?(head_new_pos, tail_visited.last) && !together?(head_new_pos, tail_visited.last)
+        tail_new_pos = [tail_visited.last[0], tail_visited.last[1] - 1]
+      elsif together?(head_new_pos, tail_visited.last)
+        tail_new_pos = tail_visited.last
+      else
+        tail_new_pos = tail_new_diag_pos(head_new_pos, tail_visited.last)
+      end
+      
+      head_visited << head_new_pos
+      tail_visited << tail_new_pos
+    end
+  end
+end
+
+puts tail_visited.uniq.size
+
+# part 2
+
+
+
+def tail_new_vertical_pos(head_pos, tail_pos)
+  y_head = head_pos[1]
+  y_tail = tail_pos[1]
+
+  return [tail_pos[0], tail_pos[1] + 1] if y_head > y_tail
+  return [tail_pos[0], tail_pos[1] - 1] if y_head < y_tail
+end
+
+def tail_new_horizontal_pos(head_pos, tail_pos)
+  x_head = head_pos[0]
+  x_tail = tail_pos[0]
+
+  return [tail_pos[0] + 1, tail_pos[1]] if x_head > x_tail
+  return [tail_pos[0] - 1, tail_pos[1]] if x_head < x_tail
+end
+
+NUMBER_OF_KNOTS = 10
+knot_pos = Array.new(NUMBER_OF_KNOTS, START)
+tail_visited = [START]
+
+motions.each do |motion|
+  direction = motion.first
+  steps = motion.last
+  
+  if direction == 'R'
+    steps.times do
+      knot_pos[0] = [knot_pos.first[0] + 1, knot_pos.first[1]] # head
+
+      (1...knot_pos.size).each do |index|
+        if horizontal?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_horizontal_pos(knot_pos[index-1],knot_pos[index])
+        elsif vertical?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_vertical_pos(knot_pos[index-1],knot_pos[index])
+        elsif together?(knot_pos[index-1], knot_pos[index])
+          next
+        else
+          knot_pos[index] = tail_new_diag_pos(knot_pos[index-1], knot_pos[index])
+        end
+
+        tail_visited << knot_pos[index] if index == knot_pos.size - 1    
+      end
+    end
+  elsif direction == 'L'
+    steps.times do
+      knot_pos[0] = [knot_pos.first[0] - 1, knot_pos.first[1]] # head
+
+      (1...knot_pos.size).each do |index|
+        if horizontal?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] =  tail_new_horizontal_pos(knot_pos[index-1],knot_pos[index])
+        elsif vertical?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_vertical_pos(knot_pos[index-1],knot_pos[index])
+        elsif together?(knot_pos[index-1], knot_pos[index])
+          next
+        else
+          knot_pos[index] = tail_new_diag_pos(knot_pos[index-1], knot_pos[index])
+        end
+
+        tail_visited << knot_pos[index] if index == knot_pos.size - 1 
+      end
+    end
+  elsif direction == 'U'
+    steps.times do
+      knot_pos[0] = [knot_pos.first[0], knot_pos.first[1] + 1] # head
+
+      (1...knot_pos.size).each do |index|
+        if vertical?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_vertical_pos(knot_pos[index-1],knot_pos[index])
+        elsif horizontal?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_horizontal_pos(knot_pos[index-1],knot_pos[index])
+        elsif together?(knot_pos[index-1], knot_pos[index])
+          next
+        else
+          knot_pos[index] = tail_new_diag_pos(knot_pos[index-1], knot_pos[index])
+        end
+
+        tail_visited << knot_pos[index] if index == knot_pos.size - 1 
+      end
+    end
+  elsif direction == 'D'
+    steps.times do
+      knot_pos[0] = [knot_pos.first[0], knot_pos.first[1] - 1] # head
+      
+      (1...knot_pos.size).each do |index|
+        if vertical?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_vertical_pos(knot_pos[index-1],knot_pos[index])
+        elsif horizontal?(knot_pos[index-1], knot_pos[index]) && !together?(knot_pos[index-1], knot_pos[index])
+          knot_pos[index] = tail_new_horizontal_pos(knot_pos[index-1],knot_pos[index])
+        elsif together?(knot_pos[index-1], knot_pos[index])
+          next
+        else
+          knot_pos[index] = tail_new_diag_pos(knot_pos[index-1], knot_pos[index])
+        end
+
+        tail_visited << knot_pos[index] if index == knot_pos.size - 1 
+      end
+    end
+  end
+end
+
+puts tail_visited.uniq.size
--- 2022/readme.md
+++ 2022/readme.md
@@ -5,16 +5,16 @@
 ## Completion Status
 
 | Day | Part 1 | Part 2 |
-| :---: | :---: | :---: |
-| 1 | :star: | :star: |
-| 2 | :star: | :star: |
-| 3 | :star: | :star: |
-| 4 | :star: | :star: |
-| 5 | :star: | :star: |
-| 6 | :star: | :star: |
-| 7 | :star: | :star: |
-| 8 | :star: | :star: |
-| 9 | | |
+| --- | :---: | :---: |
+| [1 Calorie Counting](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day01/day01.rb) | :star: | :star: |
+| [2 Rock Paper Scissors](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day02/day02.rb) | :star: | :star: |
+| [3 Rucksack Reorganization](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day03/day03.rb) | :star: | :star: |
+| [4 Camp Cleanup](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day04/day04.rb) | :star: | :star: |
+| [5 Supply Stacks](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day05/day05.rb) | :star: | :star: |
+| [6 Tuning Trouble](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day06/day06.rb) | :star: | :star: |
+| [7 No Space Left On Device](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day07/day07.rb) | :star: | :star: |
+| [8 Treetop Tree House](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day08/day08.rb) | :star: | :star: |
+| [9 Rope Bridge](https://github.com/tsangsiu/Advent_of_Code/blob/main/2022/Day09/day09.rb) | :star: | :star: |
 | 10 | | |
 | 11 | | |
 | 12 | | |
