b602123380962fcf4c1dc3e7b4b76c8c5def263c
tsangsiu <tsangsiu@gmail.com>
2022-12-18 10:09:18 +0800

    Modify Day 16 program file to come back later

--- 2022/Day16/day16.rb
+++ 2022/Day16/day16.rb
@@ -1,23 +1,3 @@
-class Graph
-  attr_accessor :map
-
-  def initialize
-    @map = {}
-  end
-
-  def add_vertex(vertex)
-    @map[vertex] = [] unless @map.key?(vertex)
-  end
-
-  def add_adjacent_vertex(vertex, *adjacent_vertex)
-    adjacent_vertex.each do |new_adjacent_vertex|
-      add_vertex(vertex) unless @map.key?(vertex)
-      add_vertex(new_adjacent_vertex) unless @map.key?(new_adjacent_vertex)
-      @map[vertex] << new_adjacent_vertex unless @map[vertex].include?(new_adjacent_vertex)
-    end
-  end
-end
-
 ### input
 
 input = File.read("inputex.txt").split("\n")
@@ -33,24 +13,11 @@ valves.each do |valve|
   flow_rates[valve[:valve]] = valve[:flow_rate]
 end
 
+graph = {}
+valves.each do |valve|
+  graph[valve[:valve]] = valve[:to]
+end
+
 START = "AA"
 
-# valves.each do |valve|
-#   p valve
-# end
-
 ### part 1
-
-graph = Graph.new
-valves.each do |valve|
-  graph.add_adjacent_vertex(valve[:valve], *valve[:to])
-end
-
-# p graph.map
-
-# breadth-first search
-paths = []; queue = []; opened = []
-queue << [START]
-until queue.empty?
-
-end
