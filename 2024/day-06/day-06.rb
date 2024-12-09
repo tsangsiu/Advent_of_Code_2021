c81f29ae51bdc5256114c6bb0b5a64a9c56a2d3c
Jason Tsang <62657905+tsangsiu@users.noreply.github.com>
2024-12-09 02:43:24 +0000

    Done 2024 day 6 (part 2)

--- 2024/day-06/day-06.rb
+++ 2024/day-06/day-06.rb
@@ -16,7 +16,7 @@ map.each_with_index do |row, y|
   pos = init_pos
 end
 
-been_to = [pos]
+pos_been_to = [pos]
 loop do
   next_pos = pos + dir
   break unless (0..X_MAX).include?(next_pos.real) && (0..Y_MAX).include?(next_pos.imag)
@@ -24,11 +24,37 @@ loop do
     dir *= Complex(0, -1)
   else
     pos += dir
-    been_to << pos
+    pos_been_to << pos
   end
 end
 
-been_to.uniq!
-puts been_to.size
+pos_been_to.uniq!
+puts pos_been_to.size
 
 # Part 2
+
+N_POS = map.first.length * map.size
+
+count = 0
+pos_been_to.each do |guard_pos|
+  _pos_been_to = []
+  pos = init_pos
+  dir = Complex(0, 1)
+  loop do
+    next_pos = pos + dir
+    break unless (0..X_MAX).include?(next_pos.real) && (0..Y_MAX).include?(next_pos.imag)
+    if map[Y_MAX - next_pos.imag][next_pos.real] == "#" || (next_pos.real == guard_pos.real && next_pos.imag == guard_pos.imag) then
+      dir *= Complex(0, -1)
+    else
+      pos += dir
+      _pos_been_to << pos
+    end
+
+    if _pos_been_to.size > N_POS
+      count += 1
+      break
+    end
+  end
+end
+
+puts count
